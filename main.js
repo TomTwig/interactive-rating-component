5e919801052f50354f6519405ffbeadc5c9b580a
Tom Rosenzweig <tom.rosenzweig@live.de>
2022-09-05 21:01:03 +0200

    javascript done

--- main.js
+++ main.js
@@ -1,34 +1,31 @@
 const thanksState = document.getElementById('state-thanks');
 const ratingState = document.getElementById('state-rating');
-
+const userRating = document.getElementById('user__rating');
 const ratingList = document.getElementById("list_rating");
 
 let ratingValue;
+
 ratingList.addEventListener("click",(element)=>
 {
-
-
         if(element.target.tagName === "LI"){
+
+                for(let i = 0; i < element.target.parentElement.children.length; i++ ){
+                        console.log(element.target.parentElement.children[i].classList.remove("active"));
+                }
 
                 element.target.classList.toggle("active");
                 ratingValue = element.target.innerHTML;
-
-                console.log(ratingValue);
-
-
+                console.log(ratingValue); 
         }
 
 
-    
 }
 );
 
 
 
 const button = document.getElementById("button");
-
 thanksState.style.display="none";
-
 button.addEventListener("click",changeState);
 
 
@@ -38,11 +35,9 @@ button.addEventListener("click",changeState);
 
 
 function changeState (){
-
         thanksState.style.display = "block";
         ratingState.style.display = "none";
-        isRatingStage = false;
-  
+        userRating.innerHTML = ratingValue;
 }
 
 
